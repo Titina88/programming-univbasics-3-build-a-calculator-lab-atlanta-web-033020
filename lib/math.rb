b26505d53d8cf3b1660f1afeb4118562e9ce20f1
Christina Placide <christina.placide@hotmail.com>
2020-03-14 19:37:46 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -14,14 +14,16 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
-  mul = num1 * num2
+  mul = num2 * num1
   mul
 end
 
 def modulo(num1, num2)
-  mod = num1 % num2
-end
+  mod = num2 % num1
+  mod
+  end
 
 def square_root(num)
-
+  sq = num*
+  sq
 end
